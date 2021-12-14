f479535ff8af389ccf53ebaade65472db03a2041
locbh14 <buihoangloc7@gmail.com>
2021-12-14 15:46:37 +0700

    register, login, button commit

--- FrontEnd/src/pages/Login.js
+++ FrontEnd/src/pages/Login.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 
 
 import './css/Login_Register.css'
-
+import 'E:/QA-Website/FrontEnd/src/assets/fontawesome-free-5.15.4-web/js/all.js'
 
 
 class Login extends Component{
@@ -14,8 +14,9 @@ class Login extends Component{
         return(
             <div className="login-background">
                 <div className="login-container">
-                    <div className="login-content row">
-                        <div className="col-12 text-header">Login</div>
+                    <div className="login-content">
+                        <div className="col-12 text-header">QA Website</div>
+                        <div className="col-12 text-header"> Login</div>
                         <div className="col-12 form-group login-input">
                             <label>Tên đăng nhập</label>
                             <input type="text" className="form-control" placeholder="Nhập tên đăng nhập"></input>
@@ -36,12 +37,17 @@ class Login extends Component{
                             <span className="text-otherLogin"> Hoặc đăng nhập với:</span>
                         </div>
 
-                        <div className="col-12 social-login">
-                            <div className="iconGroup">
-                                <i className="fab fa-facebook social-login-icon"></i>
-                                <i className="fab fa-google social-login-icon"></i>
-                            </div>
-                            
+                        <div class="auth-form__social">
+                            <a href="" class="auth-form__social-icon--fb auth-form__social-icon--general ">
+                                <i class="auth-form__social-icon fab fa-facebook-square"></i>
+                                <span class="auth-form__title">Đăng nhập với Facebook</span>
+                                
+                            </a>
+                            <a href="" class="auth-form__social-icon--gg auth-form__social-icon--general ">
+                                <i class="auth-form__social-icon fab fa-google auth-form__social-gg"></i>
+                                <span class="auth-form__title">Đăng nhập với Google</span>
+                                
+                            </a>
                         </div>
 
                     </div>
@@ -55,3 +61,4 @@ class Login extends Component{
 
 
 export default Login
+
